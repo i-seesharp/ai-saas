c85d843a7a8cefa827d6ca55cf48cfe325c2aad3
Aaranya Dev Barman <aaranyadevbarman@Aaranyas-MacBook-Pro-2.local>
2024-03-30 05:39:25 +0300

    add code until dashboard creation

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function LandingPage() {
+    return (
+        <div>
+            <p>Landing Page (Unprotected)</p>
+            <Link href="/sign-in">
+                <Button>
+                    Login
+                </Button>
+            </Link>
+            <Link href="/sign-up">
+                <Button>
+                    Register
+                </Button>
+            </Link>
+        </div>
+    )
+}
